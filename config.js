4e0b1577d3f1e5d56297488944fcaa7d45bac183
hiepdaigia98 <91863296+hiepdaigia98@users.noreply.github.com>
2021-10-04 15:22:00 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
+    introTitle: 'Em yêu à !',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
+    btnIntro: 'Bấm vào đây nhé có điều bất ngờ',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
+    btnYes: 'Vâng, em thích anh lắm',
     btnNo: 'Không, Anh trai à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
